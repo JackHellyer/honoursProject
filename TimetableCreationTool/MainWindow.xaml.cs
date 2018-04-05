eaa837260bf8cd9c83a08c135db2d07e57fdb4dd
Jack <Jackhellyer92@gmail.com>
2018-04-05 14:17:33 +0100

    csv working but duplicates causing issue

--- TimetableCreationTool/MainWindow.xaml.cs
+++ TimetableCreationTool/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TimetableCreationTool
 {
@@ -21,6 +14,8 @@ namespace TimetableCreationTool
     public partial class MainWindow : Window
     {
         private timetableCreationEntities dbcontext;
+        
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -37,9 +32,100 @@ namespace TimetableCreationTool
         {
             insertRoomCsv irc = new insertRoomCsv();
 
-            bool? result = irc.ShowDialog();
+            //bool? result = irc.ShowDialog();
+            string csvFilePath = @"C:\Users\Jack\Desktop\test.txt";
+            DataTable csvData = getDataTableCSVFile(csvFilePath);
+
+            //DataTable csvData = getDataTableCSVFile(@"C:\Users\Jack\Desktop\test.txt");
+            InsertDataTableToSQL(csvData);
+            listView.ItemsSource = csvData.DefaultView;
+          
+            
+
+
+
 
             
+        }
+
+
+        public DataTable getDataTableCSVFile(string filePath)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(filePath))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] columnFields = csvReader.ReadFields();
+                    foreach(string column in columnFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while(!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        // making empty value as null
+                        for(int i=0; i < fieldData.Length; i++)
+                        {
+                            if(fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+
+                        }
+                        csvData.Rows.Add(fieldData);
+
+                    }
+                }
+            }
+            catch( Exception ex)
+            {
+                MessageBox.Show("not working");
+                MessageBox.Show(ex.Message);
+
+            }
+            return csvData;
+        }
+
+        static void InsertDataTableToSQL(DataTable csvFileData)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;  Initial Catalog = timetableCreation; Integrated Security = True; Connect Timeout = 30"))
+            {
+
+                
+                dbConnection.Open();
+                if(dbConnection.State == ConnectionState.Open)
+                {
+
+                    MessageBox.Show("success");
+                    using (SqlBulkCopy sbc = new SqlBulkCopy(dbConnection))
+                    {
+                        // change this method later to have a string parameter which will hold the destination table
+                        sbc.DestinationTableName = "dbo.Room";
+
+                        foreach (var column in csvFileData.Columns)
+                     
+                        sbc.ColumnMappings.Add(column.ToString(), column.ToString());
+                        sbc.WriteToServer(csvFileData);
+                        
+                        
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("failed");
+                }
+                    
+                
+                
+                
+                
+            }
         }
 
         
