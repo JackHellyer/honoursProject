3e7e89c4f87f0ccdc1d9d0c00975dbc61747d4dc
Jack <Jackhellyer92@gmail.com>
2018-04-06 23:45:46 +0100

    load timetable name and file choser working

--- TimetableCreationTool/MainWindow.xaml.cs
+++ TimetableCreationTool/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
+
 namespace TimetableCreationTool
 {
     /// <summary>
@@ -15,15 +16,15 @@ namespace TimetableCreationTool
     /// </summary>
     public partial class MainWindow : Window
     {
+
         
-        
-        
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+        public string userMyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public void newTimetable_Click(object sender, RoutedEventArgs e)
         {
             newTimetableDialog nt = new newTimetableDialog();
@@ -36,23 +37,72 @@ namespace TimetableCreationTool
         public void loadTimetable_Click(object sender, RoutedEventArgs e)
         {
 
+            using (var fbd = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                
+                fbd.RootFolder = Environment.SpecialFolder.Desktop;
+                fbd.SelectedPath = userMyDocumentsPath + @"\" + "Timetable App";
+                System.Windows.Forms.DialogResult result = fbd.ShowDialog();
+                if(result == System.Windows.Forms.DialogResult.OK)
+                {
+                    int index = fbd.SelectedPath.LastIndexOf(@"\");
+                    string tName = fbd.SelectedPath.Substring(index + 1);
+                    //MessageBox.Show(tName);
+                    bool ifValid = ifVaildLoadFile(fbd.SelectedPath);
+                    if(ifValid)
+                    {
+                        Window1 win1 = new Window1(tName);
+                        win1.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Timetable must be loaded from the Timetable App folder");
+                    }
+                    
+                    
+                    
+                        
+                        
+                        
+
+
+                }
+                    
+
+
+            }
+                
+
         }
+        private bool ifVaildLoadFile(string t)
+        {
+             bool ifValid = false;
+             var dir = Directory.GetDirectories(userMyDocumentsPath + @"\Timetable App\");
+             foreach (var i in dir)
+             { 
+                //MessageBox.Show("i:" + i);
+                if (t == i)
+                {
+                    
+                    
+                    ifValid = true;
+                    break;
+                    
+                
+                }
+                else
+                {
+                    ifValid = false;
+                    
+                    
+                }
+                    
 
 
-       
-
-       
-
-
-
-       
-
-        
-    
-       
-
-        
-
+             }
+             return ifValid;
+        }
 
     }
 }
